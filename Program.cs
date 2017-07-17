7063d1138772e0477c70321ed0e8b340d4b2ecd7
hemin <pinbor@iissy.com>
2017-07-17 09:49:44 +0800

    consul register for api demo

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace SchoolAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .Build();
+
+            var freePort = FreeTcpPort();
+
+            var host = new WebHostBuilder()
+                .UseConfiguration(config)
+                .UseUrls($"http://192.168.1.76:{freePort}")        
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            var loggingFactory = host.Services.GetService(typeof(ILoggerFactory)) as ILoggerFactory;
+            var logger = loggingFactory.CreateLogger(nameof(Program));
+            logger.LogInformation($"{Process.GetCurrentProcess().Id}");
+
+            host.Run();
+        }
+
+        private static int FreeTcpPort()
+        {
+            var l = new TcpListener(IPAddress.Loopback, 0);
+            l.Start();
+            var port = ((IPEndPoint)l.LocalEndpoint).Port;
+            l.Stop();
+            return port;
+        }        
+    }
+}
